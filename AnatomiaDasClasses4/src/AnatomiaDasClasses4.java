2dd3dcc8e91f68e99849460bbd149447a1d76f7c
Natã Moura <106578929+natamoura@users.noreply.github.com>
2023-02-27 04:20:22 -0300

    Adicionando exercicios

---
+++ AnatomiaDasClasses4/src/AnatomiaDasClasses4.java
@@ -0,0 +1,14 @@
+public class AnatomiaDasClasses4 {
+    public static void main(String[] args) {
+        // Identação: É deixar seu codigo de um nivel hierarquico, para melhor compreensão.
+        // OBS IMPORTANTE: Tenha no maximo 6 niveis de hierarquia, mais que isso não e aconselhavel. 
+
+        int mediaFinal = 6;
+            if (mediaFinal < 6)
+                System.out.println("Reprovado");
+            else if (mediaFinal == 6)
+                System.out.println("Prova Minerva");
+            else
+                System.out.println("Aprovado");        
+    }
+}
