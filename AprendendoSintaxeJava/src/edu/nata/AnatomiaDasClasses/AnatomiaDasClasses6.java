71cd6c56ed28f7bbf3f4bd62c2e059143175a95a
Natã Moura <106578929+natamoura@users.noreply.github.com>
2023-03-01 15:21:10 -0300

    Adicionando mais exercicios

---
+++ AprendendoSintaxeJava/src/edu/nata/AnatomiaDasClasses/AnatomiaDasClasses6.java
@@ -0,0 +1,19 @@
+package edu.nata.AnatomiaDasClasses;
+
+public class AnatomiaDasClasses6 {
+    public static void main(String[] args) {
+        // Formas não recomendadas de declarar variaveis
+
+            double salMedio = 1500.20; // Variavel Abreviada dificil de entender o que seria
+            String emails = "aluno@escola.com"; // so existe um valor
+            String myName = "Joseth" // Use apenas uma idioma para escrever as variaveis
+         
+        // Formas recomendadas
+            double salarioMedio = 1500.23;
+            String email = "aluno@escola.com";
+            String [] emaails =  {"aluno@escola.com","professor@escola.com"}; 
+            String meuNome = "JOSEPH";
+            
+        // OBS: Tenha um metodo apenas para uma ação, exemplo: um metodo que calcula variaveis e outro que imprima.    
+    }
+}
