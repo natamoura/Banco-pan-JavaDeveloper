b89dfe7fd6973fa858b4cd8bb1fa3c28f0978e4e
Natã Moura <106578929+natamoura@users.noreply.github.com>
2023-03-01 15:54:16 -0300

    Adicionando Tipos e variaveis

---
+++ AprendendoSintaxeJava/src/edu/nata/TiposeVariaveis/TiposeVariaveis.java
@@ -0,0 +1,13 @@
+package edu.nata.TiposeVariaveis;
+
+public class TiposeVariaveis {
+    public static void main(String[] args) {
+        // numericos inteiros e decimais
+            byte idade = 123;
+            short ano = 2021;
+            int cep = 21070333;
+            long cpf = 98765432109L;
+            float pi = 3.14F;
+            double salario = 1275.33;
+    }
+}
